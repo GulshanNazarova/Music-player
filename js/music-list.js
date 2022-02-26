7f519b41022eeae6338d93b8183622dac578a8d8
ganfnezerli <gulnezerli98@gmail.com>
2022-02-26 18:42:16 +0400

    ilk commit

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic=[
+    {
+        name: "Ash - Dans La Peau",
+        artist: "Ash & Amelie Martinez ",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "MEIMUNA - La tristesse du diable",
+        artist: "Meimuna",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "No land - Niyə belə uzundur bu yollar",
+        artist: "No land",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Tamino - İndigo night",
+        artist: "Tamino",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "The moderator-words remain",
+        artist: "The moderator",
+        img: "music-5",
+        src: "music-5"
+    },
+];
